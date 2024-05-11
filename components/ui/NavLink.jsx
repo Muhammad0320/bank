5d5bc7b374c0bec0a644ee1df7e193fe6389e302
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-11 05:13:32 +0100

    chore:  update navlinl

---
+++ components/ui/NavLink.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+const StyledNavLink = styled(Link)`
+    
+    font-size:  ${() => clampBuilder(320, 1200, 1.3, 2.2)};
+
+color: var(--text-color); 
+
+&:hover,
+.active{    
+
+    
+
+    background-color: var(--card-color) ;
+
+    border-radius: 3.6rem;
+
+    padding-inline:  ${() => clampBuilder(320, 1200, 1.4, 2)};
+    padding-inline:  ${() => clampBuilder(320, 1200, 1, 1.4)};
+
+}
+
+
+`;
+
+
+function NavLink({href, children}) {
+
+    const path = usePathname()
+
+    const isActive = path === href ? 'active' : '' ;
+
+    
+
+    return (
+        <StyledNavLink href={href} className={isActive} >
+            {children}
+        </StyledNavLink>
+    )
+}
+
+export default NavLink
