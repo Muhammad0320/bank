731e55786773eac83d123d854fa45c5926041c98
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-14 15:18:36 +0100

    chore: add icons

--- components/Home/products/UseCases.jsx
+++ components/Home/products/UseCases.jsx
@@ -2,6 +2,11 @@
 
 import { ClampComponent } from '../../../styles/clampBuilder';
 import { Text } from '../../ui/Text';
+import Future from '/public/assets/icons/future.svg';
+import Home from '/public/assets/icons/home.svg';
+import Finance from '/public/assets/icons/finance.svg';
+import Edu from '/public/assets/icons/finance.svg';
+
 import { TextGradient } from '../../ui/TextGradient';
 import { HeadingWrapper } from './HeadingWrapper';
 
--- src/app/(marketing)/page.jsx
+++ src/app/(marketing)/page.jsx
@@ -1,11 +1,13 @@
 import Header from '../../../components/Home/header/Header';
 import Products from '../../../components/Home/products/Products';
+import UseCases from '../../../components/Home/products/UseCases';
 
 function Page() {
   return (
     <>
       <Header />
       <Products />
+      <UseCases />
     </>
   );
 }
