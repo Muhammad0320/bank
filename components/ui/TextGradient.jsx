262af05f216050253d65ad1816a42d660dc1b2fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-14 08:31:12 +0100

    chore: add text gradient

---
+++ components/ui/TextGradient.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const StyledText = styled.div`
+  font-size: inherit;
+
+  font-weight: 650;
+
+  background-color: var(--primary-gradient);
+  background-clip: text;
+
+  -webkit-background-clip: text;
+
+  color: transparent;
+`;
+
+function TextGradient() {
+  return <span></span>;
+}
+
+export default TextGradient;
