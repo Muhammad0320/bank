7047aa416cb0209a998f8d968423c54ee636e315
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-15 07:41:05 +0100

    chore: add feture comp to page

--- src/app/(marketing)/page.jsx
+++ src/app/(marketing)/page.jsx
@@ -1,3 +1,4 @@
+import Features from '../../../components/Home/features/Features';
 import Header from '../../../components/Home/header/Header';
 import Products from '../../../components/Home/products/Products';
 import UseCases from '../../../components/Home/products/UseCases';
@@ -8,6 +9,7 @@ function Page() {
       <Header />
       <Products />
       <UseCases />
+      <Features />
     </>
   );
 }
