bb0ffcfc7d9aa7ae86c0f8b2d2700365aaa04580
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-16 08:57:18 +0100

    chore: update page

--- src/app/(marketing)/page.jsx
+++ src/app/(marketing)/page.jsx
@@ -3,6 +3,7 @@ import Header from '../../../components/Home/header/Header';
 import Products from '../../../components/Home/products/Products';
 import UseCases from '../../../components/Home/products/UseCases';
 import Features from '../../../components/Home/features/Features';
+import Testimonials from '../../../components/Home/testimonials/Testimonials';
 
 function Page() {
   return (
@@ -12,6 +13,7 @@ function Page() {
       <UseCases />
       <Features />
       <Faq />
+      <Testimonials />
     </>
   );
 }
