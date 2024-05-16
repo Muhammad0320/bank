587ffaa4c4ecc8afa6e783b67ac42645275cc77d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-16 08:55:54 +0100

    chore: add mask image

--- components/Home/testimonials/Testimonials.jsx
+++ components/Home/testimonials/Testimonials.jsx
@@ -1,9 +1,13 @@
 'use client';
 
 import styled from 'styled-components';
+import Goto from '/public/assets/icons/goto.svg';
+import Apos from '/public/assets/icons/apos.svg';
 import HeadingWrapper from '../../ui/HeadingWrapper';
 import { SectionContainer } from '../../ui/SectionContainer';
 import { ClampComponent } from '../../../styles/clampBuilder';
+import { Text } from '../../ui/Text';
+import { TextGradient } from '../../ui/TextGradient';
 
 const ContentContainer = styled.div`
   display: grid;
@@ -18,41 +22,38 @@ const ContentContainer = styled.div`
 `;
 
 const IconContainer = styled.span`
-    
-    
-    width: 4rem;
-        height: 4rem;
+  width: 4rem;
+  height: 4rem;
 
-        display: flex;
+  display: flex;
 
+  justify-content: center;
 
-        justify-content: center;
+  align-items: center;
 
-        align-items: center; 
+  & > svg:first-of-type {
+    transform: rotateX(45deg);
+  }
 
-
-        & > svg:first-of-type {
-
-            transform: rotateX(45deg); 
-
-
-}
-
-
-& > svg: :last-of-type {
-
-transform: rotateX(-45deg); 
-
-
-}
-
-
+  & > svg:last-of-type {
+    transform: rotateX(-45deg);
+  }
 `;
 
 const TestimonialsContainer = styled.div`
   display: flex;
 
   gap: ${() => ClampComponent(320, 1200, 4, 6)};
+
+  mask-image:
+    linear-gradient(90deg, rgb(0 0 0), rgb(0 0 0 / 0.2) 10%, rgb(0 0 0 / 0.2))
+      90%,
+    rgb(0 0 0);
+
+  -webkit-mask-image:
+    linear-gradient(90deg, rgb(0 0 0), rgb(0 0 0 / 0.2) 10%, rgb(0 0 0 / 0.2))
+      90%,
+    rgb(0 0 0);
 
   flex-wrap: nowrap;
 
@@ -91,7 +92,57 @@ function Testimonials() {
         us for a secure and prosperous financial journey
       </HeadingWrapper>
 
-      <ContentContainer></ContentContainer>
+      <ContentContainer>
+        <IconContainer>
+          <Goto />
+        </IconContainer>
+
+        <TestimonialsContainer>
+          <TestimonialWrapper>
+            <Apos />
+            <Text>
+              I recently started my own business, and YourBank has been
+              instrumental in helping me set up my business accounts and secure
+              the financing I needed. Their expert guidance and tailored
+              solutions have been invaluable.
+            </Text>
+          </TestimonialWrapper>
+
+          <TextGradient> Lisan alGaib </TextGradient>
+        </TestimonialsContainer>
+
+        <TestimonialsContainer>
+          <TestimonialWrapper>
+            <Apos />
+            <Text>
+              I recently started my own business, and YourBank has been
+              instrumental in helping me set up my business accounts and secure
+              the financing I needed. Their expert guidance and tailored
+              solutions have been invaluable.
+            </Text>
+          </TestimonialWrapper>
+
+          <TextGradient> Andrew Tate </TextGradient>
+        </TestimonialsContainer>
+
+        <TestimonialsContainer>
+          <TestimonialWrapper>
+            <Apos />
+            <Text>
+              I recently started my own business, and YourBank has been
+              instrumental in helping me set up my business accounts and secure
+              the financing I needed. Their expert guidance and tailored
+              solutions have been invaluable.
+            </Text>
+          </TestimonialWrapper>
+
+          <TextGradient> Shit man </TextGradient>
+        </TestimonialsContainer>
+
+        <IconContainer>
+          <Goto />
+        </IconContainer>
+      </ContentContainer>
     </SectionContainer>
   );
 }
