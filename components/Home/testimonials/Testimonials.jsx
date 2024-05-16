d08358efd3f32b165fd42c7556cfd2dffa5b43f5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-16 08:26:36 +0100

    chore: update testimonials

---
+++ components/Home/testimonials/Testimonials.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import HeadingWrapper from '../../ui/HeadingWrapper';
+import { SectionContainer } from '../../ui/SectionContainer';
+
+function Testimonials() {
+  return (
+    <SectionContainer>
+      <HeadingWrapper withGradient="Testimonials" withoutGradient="our">
+        Discover how YourBank has transformed lives with innovative digital
+        solutions and personalized customer service. See why our clients trust
+        us for a secure and prosperous financial journey
+      </HeadingWrapper>
+    </SectionContainer>
+  );
+}
+
+export default Testimonials;
