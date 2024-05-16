bb2b331ebb33d7b7191b820c8d6b6eb2b90ec866
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-16 05:16:59 +0100

    chore: update faq

--- src/app/(marketing)/page.jsx
+++ src/app/(marketing)/page.jsx
@@ -1,7 +1,8 @@
-import Features from '../../../components/Home/features/Features';
+import Faq from '../../../components/Home/faq/Faq';
 import Header from '../../../components/Home/header/Header';
 import Products from '../../../components/Home/products/Products';
 import UseCases from '../../../components/Home/products/UseCases';
+import Features from '../../../components/Home/features/Features';
 
 function Page() {
   return (
@@ -10,6 +11,7 @@ function Page() {
       <Products />
       <UseCases />
       <Features />
+      <Faq />
     </>
   );
 }
