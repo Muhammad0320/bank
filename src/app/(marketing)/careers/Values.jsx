3869dcdccdc42fc4d89833422e6771c2263ef4cb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-18 12:59:02 +0100

    chore: update footer

--- src/app/(marketing)/careers/Values.jsx
+++ src/app/(marketing)/careers/Values.jsx
@@ -49,9 +49,9 @@ function Values() {
       <HeadingTextContainer withGradient="Values" withoutGradient="Our">
         At YourBank, our values form the foundation of our organization and
         guide our actions. We believe in upholding the highest standards of
-        integrity, delivering exceptional service, and embracing innovation.
-        These values define our culture and shape the way we work together to
-        achieve our goals.
+        integrity, <br /> delivering exceptional service, and embracing
+        innovation. These values define our culture and shape the way we work
+        together to achieve our goals.
       </HeadingTextContainer>
 
       <ValuesContainerList>
