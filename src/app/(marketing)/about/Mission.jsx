afa6ac3d22b5d455ee4ad3de2a70110af56e31d4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-19 18:47:32 +0100

    chore: update ocde

--- src/app/(marketing)/about/Mission.jsx
+++ src/app/(marketing)/about/Mission.jsx
@@ -2,6 +2,11 @@ import styled, { css } from 'styled-components';
 import HeadingTextContainer from '../../../../components/ui/HeadingWrapper';
 import { SectionContainer } from '../../../../components/ui/SectionContainer';
 import { ClampComponent } from '../../../../styles/clampBuilder';
+import Radial from '/public/assets/icons/radial.svg';
+import Image from 'next/image';
+import { ColumnFlex } from '../../../../components/ui/flex';
+import { H3 } from '../../../../components/ui/H2';
+import { Text } from '../../../../components/ui/Text';
 
 const ContentContainer = styled.div`
   display: flex;
@@ -52,7 +57,13 @@ const ImageContainer = styled.div`
   & > image {
     align-self: flex-end;
 
-    border-radius: 2rem 2rem 0 0;
+    max-width: 100%;
+
+    height: auto;
+
+    object-fit: cover;
+
+    /* border-radius: 2rem 2rem 0 0; */
   }
 
   &::after {
@@ -91,7 +102,59 @@ function Mission() {
         customer satisfaction and community development
       </HeadingTextContainer>
 
-      <ContentContainer></ContentContainer>
+      <ContentContainer>
+        <ImageContainer>
+          <AbstractContainer>
+            <Radial />
+          </AbstractContainer>
+
+          <Image
+            src="/assets/images/mission.png"
+            alt="A plant growing from coins"
+            quality={75}
+            layout="fill"
+          />
+        </ImageContainer>
+
+        <ColumnFlex>
+          <H3> Mission </H3>
+          <Text color="dark">
+            At YourBank, our mission is to empower our customers to achieve
+            financial success. We are dedicated to delivering innovative banking
+            solutions that cater to their unique needs. Through personalized
+            services, expert guidance, and cutting-edge technology, we aim to
+            build strong, lasting relationships with our customers. Our mission
+            is to be their trusted partner, helping them navigate their
+            financial journey and realize their dreams.
+          </Text>
+        </ColumnFlex>
+
+        <ImageContainer>
+          <AbstractContainer>
+            <Radial />
+          </AbstractContainer>
+
+          <Image
+            src="/assets/images/vision.png"
+            alt="A plant growing from coins"
+            quality={75}
+            layout="fill"
+          />
+        </ImageContainer>
+
+        <ColumnFlex>
+          <H3> Mission </H3>
+          <Text color="dark">
+            At YourBank, our mission is to empower our customers to achieve
+            financial success. We are dedicated to delivering innovative banking
+            solutions that cater to their unique needs. Through personalized
+            services, expert guidance, and cutting-edge technology, we aim to
+            build strong, lasting relationships with our customers. Our mission
+            is to be their trusted partner, helping them navigate their
+            financial journey and realize their dreams.
+          </Text>
+        </ColumnFlex>
+      </ContentContainer>
     </SectionContainer>
   );
 }
