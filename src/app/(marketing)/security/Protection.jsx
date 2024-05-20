b4b0f563d7d80cde7f23d2a21ee78affba6ce3e9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-20 11:50:50 +0100

    chore: update protection

--- src/app/(marketing)/security/Protection.jsx
+++ src/app/(marketing)/security/Protection.jsx
@@ -36,23 +36,32 @@ function Protection() {
           <Sec2 />
           <H4>Multi-Factor Authentication</H4>
           <Text>
-            OTo enhance the security of your online banking experience, we
-            employ multi-factor authentication. This additional layer of
-            security requires you to provide multiple pieces of identification,
-            such as a password and a one-time verification code, to access your
-            account.
+            To enhance the security of your online banking experience, we employ
+            multi-factor authentication. This additional layer of security
+            requires you to provide multiple pieces of identification, such as a
+            password and a one-time verification code, to access your account.
           </Text>
         </Card>
 
         <Card>
-          <Sec2 />
-          <H4></H4>
+          <Sec3 />
+          <H4> Fraud Monitoring </H4>
           <Text>
-            OTo enhance the security of your online banking experience, we
-            employ multi-factor authentication. This additional layer of
-            security requires you to provide multiple pieces of identification,
-            such as a password and a one-time verification code, to access your
-            account.
+            We have sophisticated fraud detection systems in place to monitor
+            your accounts for any suspicious activities. Our dedicated team
+            works around the clock to detect and prevent unauthorized
+            transactions, providing you with peace of mind.
+          </Text>
+        </Card>
+
+        <Card>
+          <Sec4 />
+          <H4> Secure Mobile Banking </H4>
+          <Text>
+            Our mobile banking app is designed with the same level of security
+            as our online banking platform. You can confidently access your
+            accounts, make transactions, and manage your finances on the go,
+            knowing that your information is protected.
           </Text>
         </Card>
       </X4Grid>
