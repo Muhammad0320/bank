b527929576a3198b61903f61536965df955ef34d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-20 11:42:21 +0100

    chore: update protection

---
+++ src/app/(marketing)/security/Protection.jsx
@@ -0,0 +1,17 @@
+import HeadingTextContainer from '../../../../components/ui/HeadingWrapper';
+import { SectionContainer } from '../../../../components/ui/SectionContainer';
+
+function Protection() {
+  return (
+    <SectionContainer>
+      <HeadingTextContainer withGradient="Protect You" withoutGradient="How We">
+        At YourBank, we prioritize the security and confidentiality of your
+        financial information. Our state-of-the-art encryption technology and
+        stringent data protection measures ensure your assets and transactions
+        are safeguarded at all times
+      </HeadingTextContainer>
+    </SectionContainer>
+  );
+}
+
+export default Protection;
