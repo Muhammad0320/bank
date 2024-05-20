44cb555715b047fa6b8bf4f637d2991980c7a57b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-20 11:49:26 +0100

    chore: update protection

--- src/app/(marketing)/security/Protection.jsx
+++ src/app/(marketing)/security/Protection.jsx
@@ -1,5 +1,14 @@
+import { X4Grid } from '../../../../components/ui/4x4Grid';
+import Sec1 from '/public/assets/icons/sec1.svg';
+import Sec2 from '/public/assets/icons/sec2.svg';
+import Sec3 from '/public/assets/icons/sec3.svg';
+import Sec4 from '/public/assets/icons/sec4.svg';
+
+import { Card } from '../../../../components/ui/Card';
 import HeadingTextContainer from '../../../../components/ui/HeadingWrapper';
 import { SectionContainer } from '../../../../components/ui/SectionContainer';
+import { H4 } from '../../../../components/ui/H2';
+import { Text } from '../../../../components/ui/Text';
 
 function Protection() {
   return (
@@ -10,6 +19,43 @@ function Protection() {
         stringent data protection measures ensure your assets and transactions
         are safeguarded at all times
       </HeadingTextContainer>
+
+      <X4Grid>
+        <Card>
+          <Sec1 />
+          <H4>Secure Online Banking Platform</H4>
+          <Text>
+            Our online banking platform is built with multiple layers of
+            security to safeguard your information. We utilize industry-standard
+            encryption protocols to ensure that your data remains confidential
+            and protected during transmission.
+          </Text>
+        </Card>
+
+        <Card>
+          <Sec2 />
+          <H4>Multi-Factor Authentication</H4>
+          <Text>
+            OTo enhance the security of your online banking experience, we
+            employ multi-factor authentication. This additional layer of
+            security requires you to provide multiple pieces of identification,
+            such as a password and a one-time verification code, to access your
+            account.
+          </Text>
+        </Card>
+
+        <Card>
+          <Sec2 />
+          <H4></H4>
+          <Text>
+            OTo enhance the security of your online banking experience, we
+            employ multi-factor authentication. This additional layer of
+            security requires you to provide multiple pieces of identification,
+            such as a password and a one-time verification code, to access your
+            account.
+          </Text>
+        </Card>
+      </X4Grid>
     </SectionContainer>
   );
 }
