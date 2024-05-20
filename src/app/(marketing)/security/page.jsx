77503bb894f8f09f30446e1fd5ffe46bad724b38
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-20 11:52:11 +0100

    chore: update security page

--- src/app/(marketing)/security/page.jsx
+++ src/app/(marketing)/security/page.jsx
@@ -1,9 +1,13 @@
+import Faq from '../../../../components/Home/faq/Faq';
+import Protection from './Protection';
 import SecurityHero from './SecurityHero';
 
 function Security() {
   return (
     <>
       <SecurityHero />
+      <Protection />
+      <Faq />
     </>
   );
 }
