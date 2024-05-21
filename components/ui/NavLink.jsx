0d3c91e9f16e13838905712e0e50703347246dd0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-21 08:48:15 +0100

    chore:  add navlinks

--- components/ui/NavLink.jsx
+++ components/ui/NavLink.jsx
@@ -15,18 +15,46 @@ const StyledNavLink = styled(Link)`
 
   padding-block: ${() => ClampComponent(320, 1200, 0.8, 1.2)};
 
-  transition: background-color 350ms cubic-bezier(0.215, 0.61, 0.355, 1);
+  transition:
+    background-color 350ms cubic-bezier(0.215, 0.61, 0.355, 1),
+    opacity 300ms ease-in-out;
 
-  &:hover,
+  &::after {
+    width: 100%;
+
+    height: 1px;
+
+    background-image: var(--primary-gradient);
+
+    scale: 0 1;
+
+    transition:
+      scale 250ms,
+      translate 350ms;
+  }
+
+  &:hover::after,
   &.active {
-    background-color: var(--card-color);
+    /* background-color: var(--card-color); */
+
+    scale: 1 1;
+  }
+
+  &:not(:hover) {
+    opacity: 0.8;
+  }
+
+  &:hover + &::after {
+    translate: -100%;
+  }
+
+  &:has(+ :hover)::after {
+    translate: 100%;
   }
 `;
 
 function NavLink({ href, children }) {
   const path = usePathname();
-
-  console.log(path);
 
   const isActive = path === href ? 'active' : '';
 
