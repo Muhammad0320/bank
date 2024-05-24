85618a1265d9faa5f146e0e14df76c3db08b43e7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-24 19:37:52 +0100

    chore: update page

--- src/app/(marketing)/careers/page.jsx
+++ src/app/(marketing)/careers/page.jsx
@@ -5,6 +5,10 @@ import CarreerHero from './Hero';
 import Jobs from './Jobs';
 import Values from './Values';
 
+export const metadata = {
+  title: 'Careers',
+};
+
 function Carrers() {
   return (
     <>
